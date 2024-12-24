fc6f10fb4c81c99d2fc8f53ede2c9ec9baa94815
paulohlips <paulohfcmendes@gmail.com>
2024-12-24 12:28:25 -0300

    creating welcome session

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,32 @@ import backgroundImage from "../../../src/assets/images/background.png"
 
 export const Header = () => {
     return (
-        <div >
+        <div style={style}>
             <Menu />
             <Banner />
+            <WelcomeTile />
+        </div>
+    )
+}
+
+const WelcomeTile = () => {
+    return (
+        <div style={{
+            backgroundColor: '#DE9B86',
+            height: '80px', display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '5vh'
+        }}>
+            <p style={{ color: '#FFFFFF', fontSize: 64 }}>Sejam bem-vindos ao nosso site</p>
         </div>
     )
 }
 
 const Banner = () => {
     return (
-        <div style={style}>
-            <p>B IMAGE P</p>
-            <p>Beatriz e Paulo Henrique</p>
-            <p>13.09.25</p>
+        <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '20vh' }}>
+            <img style={{ maxHeight: '100vh' }} src="../../../src/assets/images/monogram.png" />
         </div>
     )
 }
@@ -24,6 +37,5 @@ const style = {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
-    height: "100vh",
 };
 
