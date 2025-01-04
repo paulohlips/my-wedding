bbafe485f5db9f2224c299fe22c46603f03ed412
paulohlips <paulohfcmendes@gmail.com>
2025-01-04 09:45:55 -0300

    WIP: gifts sections

---
+++ src/sections/Gifts.tsx
@@ -0,0 +1,9 @@
+export const Gifts = () => {
+    return (
+        <div>
+            <p>Presentes aos noivos</p>
+            <p>Fique à vontade para escolher entre uma das três opções de presente</p>
+
+        </div>
+    )
+}
