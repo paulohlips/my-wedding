2427eff7e0c1986fcc5e657735007d1217ee1ea1
paulohlips <paulohfcmendes@gmail.com>
2025-01-05 09:27:47 -0300

    feat: improve confirmation component styles

--- src/sections/PresenceConfirmation.tsx
+++ src/sections/PresenceConfirmation.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import styles from "./PresenceConfirmation.module.css"
 
 export const PresenceConfirmation = () => {
+    const [inviteAccepted, sentInviteAccepted] = useState<boolean>(false)
+
+    const handleInvited = (input: boolean) => {
+        sentInviteAccepted(input)
+    }
+
     return (
         <div className={styles.presenceConfirmation}>
             <p>Confirme sua Presença</p>
@@ -8,21 +15,34 @@ export const PresenceConfirmation = () => {
                 <p className={styles.radioSectionTitle}>Você irá comparecer à cerimônia?</p>
                 <div className={styles.radioContainer}>
                     <div className={styles.radioContainerItem}>
-                        <input type="radio" id="contactChoice1" name="contact" value="email" size={28} />
+                        <input
+                            type="radio"
+                            id="contactChoice1"
+                            name="invite" value="option"
+                            size={28}
+                            onChange={() => handleInvited(true)}
+                        />
                         <label>Sim</label>
                     </div>
 
                     <div className={styles.radioContainerItem}>
-                        <input type="radio" id="contactChoice2" name="contact" value="phone" />
+                        <input type="radio"
+                            id="contactChoice2"
+                            name="invite"
+                            value="option"
+                            onChange={() => handleInvited(false)}
+                        />
                         <label>Não</label>
                     </div>
                 </div>
 
-                {/* TODO: just render token input field if invited person confirms presence */}
-                <p>Digite seu token de confirmação: </p>
-                <input type="text" />
-
-                <button>Confirmar</button>
+                {inviteAccepted
+                    ? <div className={styles.confirmation}>
+                        <p>Digite seu token de confirmação: </p>
+                        <input type="text" />
+                        <button>Confirmar</button>
+                    </div>
+                    : null}
             </div>
         </div>
     )
