c864efd83f630a70fc31b116ff25588cbd6f22b5
paulohlips <paulohfcmendes@gmail.com>
2025-01-05 12:19:53 -0300

    feat: creating dress code section

---
+++ src/sections/DressCode.tsx
@@ -0,0 +1,16 @@
+import styles from "./DressCode.module.css"
+import dresscode1 from "../assets/images/dress-code-1.png"
+import dresscode2 from "../assets/images/dress-code-2.png"
+
+export const DressCode = () => {
+    return (
+        <div className={styles.dressCode}>
+            <p>Dress Code: <strong>Esporte Fino</strong></p>
+            <div className={styles.images}>
+                <img src={dresscode1} />
+                <img src={dresscode2} />
+            </div>
+            <h2>Dica: Usem sapatos confortáveis e sugerimos que não seja salto fino, o local é gramado</h2>
+        </div>
+    )
+}
