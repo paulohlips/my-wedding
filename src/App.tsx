16d8519ed753a74379337658fb7d2498b47c8da4
paulohlips <paulohfcmendes@gmail.com>
2025-01-05 12:20:10 -0300

    feat: creating map with location links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header/Header'
+import { DressCode } from './sections/DressCode'
 import { Fiances } from './sections/Fiances'
 import { Gifts } from './sections/Gifts'
 import { Godparents } from './sections/Godparents'
 import { Messages } from './sections/Messages'
 import { MessageToFiances } from './sections/MessageToFiances'
 import { PresenceConfirmation } from './sections/PresenceConfirmation'
+import { Map } from './sections/Map/Map'
 
 export default function App() {
   return (
@@ -15,7 +17,9 @@ export default function App() {
       <MessageToFiances />
       <Messages />
       <PresenceConfirmation />
+      <DressCode />
       <Gifts />
+      <Map />
     </div>
   )
 }
---
+++ src/sections/Map/Map.tsx
@@ -0,0 +1,54 @@
+import { IoLocationOutline } from "react-icons/io5";
+import styles from "./Map.module.css"
+import waze from "../../assets/images/waze.webp"
+import googleMaps from "../../assets/images/google-maps.png"
+
+export const Map = () => {
+    const address = "-15.674549019018466, -48.09409136933495"; // Replace with the fixed address
+    const mapUrl = `https://www.google.com/maps?q=${encodeURIComponent(address)}&output=embed`;
+
+    const encodedAddress = encodeURIComponent(address);
+
+    const googleMapsLink = `https://www.google.com/maps?q=${encodedAddress}`;
+    const wazeLink = `https://waze.com/ul?q=${encodedAddress}`;
+
+    return (
+        <div>
+            <div className={styles.map}>
+                <p>Local do Evento</p>
+
+                <div className={styles.address}>
+                    <div className={styles.locationIcon}>
+                        <IoLocationOutline size={48} color="#A84424" />
+                    </div>
+                    <div>
+                        <p><strong>Recanto das Flores</strong></p>
+                        <p>Núcleo Rural Alexandre Gusmão, 97 - Brazlândia</p>
+                        <p>Brasília - DF, 72465-370</p>
+                    </div>
+
+                    <div className={styles.link}>
+                        <a href={googleMapsLink} target="_blank" rel="noopener noreferrer">
+                            <p>Abrir no Google Maps</p>
+                            <img src={googleMaps} />
+                        </a>
+                        <a href={wazeLink} target="_blank" rel="noopener noreferrer">
+                            <p>Abrir no Waze</p>
+                            <img src={waze} />
+                        </a>
+                    </div>
+                </div>
+                <iframe
+                    src={mapUrl}
+                    width="800"
+                    height="600"
+                    style={{ border: 0 }}
+                    allowFullScreen={true}
+                    loading="lazy"
+                ></iframe>
+
+                <button onClick={() => navigator.clipboard.writeText(address)}>Copy Address</button>
+            </div>
+        </div>
+    );
+};
