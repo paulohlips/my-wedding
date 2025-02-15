4d4b218798053ed1c91bc08a777080cf395923e4
paulohlips <paulohfcmendes@gmail.com>
2025-02-15 16:16:03 -0300

    fix: updating godparents images

--- src/sections/Godparents.tsx
+++ src/sections/Godparents.tsx
@@ -3,8 +3,10 @@ import { Card } from "../components/Card/Card";
 import styles from './Godparents.module.css'
 import { BiChevronRight } from "react-icons/bi";
 import { useRef } from "react";
+import { useIsMobile } from "../hooks/useIsMobile"
 
 export const Godparents = () => {
+    const isMobile = useIsMobile()
     const slideWindowContainer = useRef<HTMLDivElement | null>(null);
 
     const handleScroll = (direction: string) => {
@@ -35,12 +37,10 @@ export const Godparents = () => {
                         mockedData.map(card => {
                             return (
                                 <Card
-                                    //TODO: fix image dimensions
                                     key={card.id}
                                     imageStyles={{
-                                        maxHeight: '30vw',
-                                        minWidth: '80vw',
-                                        maxWidth: '100%',
+                                        maxHeight: isMobile ? '40vh' : '70vh',
+                                        minWidth: isMobile ? '100%' : '5vw',
                                         padding: '1rem'
                                     }}
                                     image={card.src}
@@ -60,32 +60,32 @@ export const Godparents = () => {
 const mockedData = [
     {
         id: 100 * Math.random(),
-        src: "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+        src: "src/assets/images/godparents-isla.jpeg",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur. Impedit laboriosam reiciendis veniam facere cupiditate dolor. Ex odio, rem esse aspernatur voluptatum officia quibusdam repellendus, est, natus unde iusto!"
     },
     {
         id: 100 * Math.random(),
-        src: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        src: "src/assets/images/godparents-isla.jpeg",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur. Impedit laboriosam reiciendis veniam facere cupiditate dolor. Ex odio, rem esse aspernatur voluptatum officia quibusdam repellendus, est, natus unde iusto!"
     },
     {
         id: 100 * Math.random(),
-        src: "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        src: "src/assets/images/godparents-isla.jpeg",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur. Impedit laboriosam reiciendis veniam facere cupiditate dolor. Ex odio, rem esse aspernatur voluptatum officia quibusdam repellendus, est, natus unde iusto!"
     },
     {
         id: 100 * Math.random(),
-        src: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        src: "src/assets/images/godparents-isla.jpeg",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur. Impedit laboriosam reiciendis veniam facere cupiditate dolor. Ex odio, rem esse aspernatur voluptatum officia quibusdam repellendus, est, natus unde iusto!"
     },
     {
         id: 100 * Math.random(),
-        src: "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        src: "src/assets/images/godparents-isla.jpeg",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur. Impedit laboriosam reiciendis veniam facere cupiditate dolor. Ex odio, rem esse aspernatur voluptatum officia quibusdam repellendus, est, natus unde iusto!"
     },
     {
         id: 100 * Math.random(),
-        src: "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        src: "src/assets/images/godparents-isla.jpeg",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur. Impedit laboriosam reiciendis veniam facere cupiditate dolor. Ex odio, rem esse aspernatur voluptatum officia quibusdam repellendus, est, natus unde iusto!"
     }
 ];
