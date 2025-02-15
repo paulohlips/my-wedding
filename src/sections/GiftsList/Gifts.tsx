c3b5c0bdf1e8e2e7aabaa7104d998512f6be1599
paulohlips <paulohfcmendes@gmail.com>
2025-02-15 09:29:42 -0300

    feat: creating new gifts sections

--- src/sections/GiftsList/Gifts.tsx
+++ src/sections/GiftsList/Gifts.tsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import { Card } from '../../components/Card/Card';
 import styles from './Gifts.module.css'
 
 export const Gifts = () => {
+    const [giftsList, setGiftsList] = useState(mockedGiftsList)
     return (
         <div className={styles.giftsSection}>
             <h1>Presentes aos noivos</h1>
             <h2>Fique à vontade para escolher entre uma das três opções de presente</h2>
             <div className={styles.giftsSectionButtons}>
-                <button>Presentes Virtuais</button>
-                <button>Presentes em Loja Física</button>
-                <button>Cotas de Lua de Mel</button>
+                <button onClick={() => setGiftsList(mockedGiftsList)}>Presentes Virtuais</button>
+                <button onClick={() => setGiftsList(mockedHoneyMoon)}>Presentes em Loja Física</button>
+                <button onClick={() => setGiftsList(mockedPhysicalStores)}>Cotas de Lua de Mel</button>
             </div>
 
             <div className={styles.disclaimer}>
@@ -17,7 +19,7 @@ export const Gifts = () => {
             </div>
             <div className={styles.gridContainer}>
                 {
-                    mockedData.map(card => {
+                    giftsList.map(card => {
                         return (
                             <Card
                                 key={card.id}
@@ -43,7 +45,7 @@ export const Gifts = () => {
     )
 }
 
-const mockedData = [
+const mockedGiftsList = [
     {
         id: 100 * Math.random(),
         src: "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
@@ -80,6 +82,150 @@ const mockedData = [
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         price: "R$ 235,90"
     },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    }
+];
+
+const mockedHoneyMoon = [
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    },
+    {
+        id: 100 * Math.random(),
+        src: "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        price: "R$ 235,90"
+    }
+];
+
+const mockedPhysicalStores = [
     {
         id: 100 * Math.random(),
         src: "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
